5dcc4ed6b441dd6ca6baf0947f0af11635c5bb97
Sergio Carrera Amorebieta <sergio.txamo@gmail.com>
2020-01-07 17:11:47 +0100

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Rutas
+var appRoutes = require('./routes/routes');
+var loginRoutes = require('./routes/login');
+var usuarioRoutes = require('./routes/usuario');
 
 // Conexión a la BD Mongo
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -13,12 +25,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(8080, () => {
